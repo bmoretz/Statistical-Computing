c89db7dbce4c971889a702a57c66666f5615700f
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-11-26 16:20:52 -0500

    Computational Finance, ch1
    Computational Finance, ch1

---
+++ Mathematical Statistics/ms_ch_04.r
@@ -0,0 +1,57 @@
+N <- 1000
+Xbar <- numeric(N) # space for results
+
+for( i in 1:n)
+{
+  x <- rexp(100, rate = 1/15) # draw random sample of size 100
+  Xbar[i] <- mean(x)
+}
+
+hist(Xbar)
+
+qqnorm(Xbar)
+qqline(Xbar)
+
+mean(Xbar)
+sd(Xbar)
+
+maxY <- numeric(N)
+
+for( i in 1:N)
+{
+  y <- runif(12) # draw random sample of size 12
+  maxY[i] <- max(y)
+}
+
+hist(maxY)
+
+X <- rpois(10^4, 5) # Draw 10^4 values from Pois(5)
+Y <- rpois(10^4, 12) # Draw 10^4 values from Pois(12)
+
+W <- X + Y
+
+hist(W, prob = T) # prob = T, scales hist to 1
+lines(2:35, dpois(2:35, 17), type = "b")
+
+mean(W)
+var(W)
+
+sd(W) / sqrt(N)
+
+Xbar <- numeric(N)
+
+for( i in 1:N )
+{
+  x <- rgamma(30, shape = 5, rate = 2)
+  Xbar[i] <- mean(x)
+}
+
+hist(Xbar)
+
+qqnorm(Xbar)
+qqline(Xbar)
+
+mean(Xbar)
+sd(Xbar)
+
+mean(Xbar > 3)

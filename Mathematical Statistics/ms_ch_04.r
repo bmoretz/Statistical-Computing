31875c80ab42dcadd466f594533bec27d5572efc
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-11-29 17:33:53 -0500

    mathematical statistics, ch4
    mathematical statistics, ch4

--- Mathematical Statistics/ms_ch_04.r
+++ Mathematical Statistics/ms_ch_04.r
@@ -66,3 +66,14 @@ perm <- permutations( length(p), sample.size, repeats.allowed = T)
 d <- data.table( c1 = p[perm[, 1]], c2 = p[perm[, 2]])
 d[, m := (c1 + c2) / 2]
 
+( 3 - 5/2 ) / (sqrt(5/2^2) / sqrt(30))
+
+pnorm(2.44949, lower.tail = F)
+
+
+z <- (0.53333 - 0.5) / 0.0289
+pbinom(z, prob = .5, size = 300, lower.tail = T)
+
+pbinom(160, size = 300, prob = .5, lower.tail = T)
+
+

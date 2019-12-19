664ea6a21d05e4487522f7603a21335371268fa7
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-12-19 17:38:48 -0500

    statistical methods
    statistical methods

--- Statistical Methods/_packages.R
+++ Statistical Methods/_packages.R
@@ -1,1 +1,13 @@
-install.packages("WRS")
+# first: install dependent packages
+install.packages(c("MASS", "akima", "robustbase"))
+
+# second: install suggested packages
+install.packages(c("cobs", "robust", "mgcv", "scatterplot3d", "quantreg", "rrcov", "lars", "pwr", "trimcluster", "mc2d", "psych", "Rfit", "DepthProc", "class", "fda"))
+
+# third: install an additional package which provides some C functions
+install.packages("devtools")
+library("devtools")
+install_github("mrxiaohe/WRScpp")
+
+# fourth: install WRS
+install_github("nicebread/WRS", subdir="pkg")

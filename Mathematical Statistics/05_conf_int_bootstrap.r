43b04072312c1fd02ddb0f8c67e1436267509ef1
Brandon Moretz <bmoretz@ionicsolutions.net>
2020-01-23 15:28:28 -0500

    bootstrap work
    bootstrap work

--- Mathematical Statistics/05_conf_int_bootstrap.r
+++ Mathematical Statistics/05_conf_int_bootstrap.r
@@ -1,7 +1,31 @@
 library(data.table)
 library(ggplot2)
+library(here)
+library(boot) # alternate to verify long-hand calcs
 
-data.dir <- "D:/Projects/Statistical-Computing/datasets/"
+data.dir <- paste0(here::here(), "/datasets/")
+
+# gamma example
+
+my.sample <- rgamma(16, 1, 1/2)
+N <- 10e5
+
+my.boot <- numeric(N)
+
+for(i in 1:N)
+{
+  x <- sample(x = my.sample, size = length(my.sample), replace = T) 
+  my.boot[i] <- mean(x)
+}
+
+ggplot(data.table(value = my.sample), aes(value, fill = ..count..)) +
+  geom_histogram(bins = 8)
+
+ggplot(data.table(value = my.boot), aes(value, fill = ..count..)) +
+  geom_histogram(bins = 30)
+
+mean(my.boot)
+sd(my.boot)
 
 ### Bangladesh
 
@@ -9,6 +33,8 @@ Bangladesh <- data.table(read.csv(paste0(data.dir, "Bangladesh.csv"),
                                  header = T))
 
 Arsenic <- Bangladesh$Arsenic
+
+mean(Arsenic) #  sample mean
 
 ggplot(data.table(value = Arsenic)) +
   geom_histogram(aes(value, fill = ..count..), bins = 30) +
@@ -46,13 +72,59 @@ mean(arsenic.mean) # Bootstrap mean
 mean(arsenic.mean) - mean(Arsenic) # bias
 sd(arsenic.mean) # bootstrap SE
 
-uq <- mean(arsenic.mean) - 1.96 * sd(arsenic.mean)
-lq <- mean(arsenic.mean) + 1.96 * sd(arsenic.mean)
+# this is not accurate: CLT cannot be used here
+lq <- mean(arsenic.mean) - 1.96 * sd(arsenic.mean)
+uq <- mean(arsenic.mean) + 1.96 * sd(arsenic.mean)
 
-sum(arsenic.mean > lq) / N
-sum(arsenic.mean < uq) / N
+sum(arsenic.mean < lq) / N
+sum(arsenic.mean > uq) / N
 
-quantile(arsenic.mean, c(0.025, 0.975)) # confidence intervals
+alpha <- .05
+quantile(arsenic.mean, c(alpha/2, 1 - alpha/2)) # confidence intervals
+
+boot.fn <- function(data, index) {
+  mean(data[index])
+}
+
+arsenic.boot <- boot(Arsenic, boot.fn, R = N)
+
+### NC birth weights
+
+NCBirths <- data.table(read.csv(paste0(data.dir, "NCBirths2004.csv"),
+                                  header = T))
+
+weights <- NCBirths$Weight
+
+boot.fn <- function(data, index) {
+  mean(data[index])  
+}
+
+bw.boot <- boot(weights, boot.fn, R = 1000)
+
+# long-hand
+
+N <- 1e5
+bw.mean <- numeric(N)
+
+for(i in 1:N)
+{
+  x <- sample(weights, size = length(weights), replace = T)
+  bw.mean[i] <- mean(x)
+}
+
+alpha <- 0.05
+conf <- quantile(bw.mean, c(Lower = alpha/2, Upper = 1 - alpha/2))
+
+# NC Birth weights, bootstrapped
+
+ggplot(data.table(values = bw.mean), aes(values)) +
+  geom_histogram(aes(fill = ..count..), bins = 30) +
+  geom_vline(xintercept = mean(weights), col = "darkorange", alpha = .4, lwd = 1) +
+  geom_vline(xintercept = conf[1], col = "cornflowerblue") +
+  geom_vline(xintercept = conf[2], col = "cornflowerblue") +
+  geom_rug(col = "darkred") +
+  scale_x_continuous(labels = scales::comma) +
+  scale_y_continuous(labels = scales::comma)
 
 ### testosterone study
 
@@ -65,7 +137,7 @@ testM <- Skateboard[Experimenter == "Male"]$Testosterone
 nf <- length(testF)
 nm <- length(testM)
 
-N <- 10e3
+N <- 10e4
 TestMean <- numeric(N)
 
 for(i in 1:N)
@@ -86,7 +158,7 @@ ggplot(data.table(value = TestMean), aes(sample = value)) +
   stat_qq_line() +
   labs(title = "Testosterone")
 
-# Statistics
+# bootstrap statistics
 
 mean(testF) - mean(testM)
 mean(TestMean)
@@ -121,11 +193,52 @@ ggplot(data.table(value = result), aes(sample = value)) +
 
 ### Verizon data
 
+# Is the average repair time lower for ILEC lower than CLEC customers?
+
 Verizon <- data.table(read.csv(paste0(data.dir, "Verizon.csv"),
                                header = T))
 
 Time.ILEC <- Verizon[Group == "ILEC"]$Time
 Time.CLEC <- Verizon[Group == "CLEC"]$Time
+
+observed <- mean(Time.ILEC) - mean(Time.CLEC)
+
+ggplot(data.table(values = Time.ILEC), aes(values)) +
+  geom_histogram(aes(y = ..density.., fill = ..count..), bins = 30) +
+  geom_density(aes(y = ..density..), col = "darkorange")
+
+ggplot(data.table(values = Time.CLEC), aes(values)) +
+  geom_histogram(aes(y = ..density.., fill = ..count..), bins = 30) +
+  geom_density(aes(y = ..density..), col = "darkorange")
+
+# bootstrap difference in means
+
+set.seed(123)
+
+N <- 10e4
+results <- numeric(N)
+
+N.ILEC <- length(Time.ILEC)
+N.CLEC <- length(Time.CLEC)
+
+for(i in 1:N)
+{
+    ilec <- sample(Time.ILEC, N.ILEC, replace = T)
+    clec <- sample(Time.CLEC, N.CLEC, replace = T)
+    
+    results[i] <- mean(ilec) - mean(clec)
+}
+
+alpha <- 0.05
+observed - mean(results) # Bias
+
+sd(results)
+
+quantile(results, c(Lower = alpha/2, Upper = 1 - alpha/2))
+
+aov(Time ~ Group, data = Verizon)
+
+# ratio of means test
 
 observed <- mean(Time.ILEC) / mean(Time.CLEC)
 

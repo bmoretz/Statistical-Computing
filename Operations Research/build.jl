b2f4b8a87d735469dcccd9ef5df0a0ae1d820917
Brandon Moretz <bmoretz82@gmail.com>
2020-05-28 18:59:51 -0400

    julia, polynomial roots
    julia, polynomial roots

--- Operations Research/build.jl
+++ Operations Research/build.jl
@@ -6,3 +6,4 @@ weave("Operations Research\\01_Introduciton.jmd", doctype=:"md2pdf", out_path=:p
 weave("02_SimpleLinearOptimization.jmd", doctype=:"github", out_path=:pwd)
 weave("03_Julia-Basics.jmd", doctype=:"github", out_path=:pwd)
 weave("03_Gadfly.jmd", doctype=:"github", out_path=:pwd)
+weave("04_Numerical-Methods.jmd", doctype=:"github", out_path=:pwd)
